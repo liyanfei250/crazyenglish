3ce613cdbbc1dfd74e9dece34b049383d7be0fa0
雷勋 <leixun33@163.com>
2023-03-01 10:37:13 +0800

    听力播放器UI修改

--- lib/widgets/WDCustomTrackShape.dart
+++ lib/widgets/WDCustomTrackShape.dart
@@ -6,23 +6,21 @@
  * Description:
  */
 import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_core/theme.dart';
+import 'package:syncfusion_flutter_sliders/sliders.dart';
 
 
-class WDCustomTrackShape extends RoundedRectSliderTrackShape {
+class WDCustomTrackShape extends SfTrackShape {
 
   WDCustomTrackShape({this.addHeight = 0});
   //增加选中滑块的高度,系统默认+2·
   double addHeight;
 
   ///去掉默认边距
-  Rect getPreferredRect({
-    required RenderBox parentBox,
-    Offset offset = Offset.zero,
-    required SliderThemeData sliderTheme,
-    bool isEnabled = false,
-    bool isDiscrete = false,
-  }) {
-    final double trackHeight = sliderTheme.trackHeight??1;
+  Rect getPreferredRect(
+      RenderBox parentBox, SfSliderThemeData themeData, Offset offset,
+      {bool? isActive}) {
+    final double trackHeight = themeData.activeTrackHeight??1;
     final double trackLeft = offset.dx;
     final double trackTop =
         offset.dy + (parentBox.size.height - trackHeight) / 2;
@@ -31,27 +29,4 @@ class WDCustomTrackShape extends RoundedRectSliderTrackShape {
   }
 
 
-  @override
-  void paint(
-      PaintingContext context,
-      Offset offset, {
-        required RenderBox parentBox,
-        required SliderThemeData sliderTheme,
-        required Animation<double> enableAnimation,
-        required TextDirection textDirection,
-        required Offset thumbCenter,
-        Offset? secondaryOffset,
-        bool isDiscrete = false,
-        bool isEnabled = false,
-        double additionalActiveTrackHeight = 0,
-      }) {
-    super.paint(context, offset, parentBox: parentBox,
-        sliderTheme: sliderTheme,
-        enableAnimation: enableAnimation,
-        textDirection: textDirection,
-        thumbCenter: thumbCenter,
-        // secondaryOffset: secondaryOffset,
-        additionalActiveTrackHeight: addHeight
-    );
-  }
 }
