97067f401bc66087aaafbe03b5b8fa2017096feb
Your Name <467638186@qq.com>
2023-03-07 19:35:32 +0800

    写作界面点击范文查看，dialog的编写

--- lib/pages/writing/writing_view.dart
+++ lib/pages/writing/writing_view.dart
@@ -1,14 +1,19 @@
+import 'package:bot_toast/bot_toast.dart';
+import 'package:crazyenglish/pages/writing/writingDialog.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_html/flutter_html.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
 
 import '../../base/AppUtil.dart';
 import '../../base/widgetPage/base_page_widget.dart';
+import '../../base/widgetPage/dialog_manager.dart';
 import '../../r.dart';
 import '../../routes/routes_utils.dart';
 import '../../utils/colors.dart';
 import '../../../entity/week_detail_response.dart' as detail;
 import 'writing_logic.dart';
+import '../../../utils/text_util.dart';
 
 class WritingPage extends BasePage {
   detail.WeekDetailResponse? testDetailResponse;
@@ -26,6 +31,10 @@ class WritingPage extends BasePage {
 class _ToOrderDetailPageState extends BasePageState<WritingPage> {
   final logic = Get.put(WritingLogic());
   final state = Get.find<WritingLogic>().state;
+  var exTile = "Everyone Favorite Fruit";
+  var exList =
+      "Everyone has favorite fruit. My favorite fruit is filled with the purple grape. It is sour. Because I like to eat sour fruit.She is a vine. The purple coat package in the body. A very beautiful. Like a purple pearls. In the sunshine. The glow of beauty. People have seen.Dear friends are you favorite fruit is what? Introduce to me ok?";
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -100,7 +109,37 @@ class _ToOrderDetailPageState extends BasePageState<WritingPage> {
               ],
             ),
             Padding(padding: EdgeInsets.only(top: 8.w)),
-            Text(
+            SelectionArea(
+              child: Html(
+                data: TextUtil.weekDetail.replaceFirst("###content###",
+                    widget.testDetailResponse?.data![0].content ?? ""),
+                onImageTap: (
+                  url,
+                  context,
+                  attributes,
+                  element,
+                ) {
+                  if (url != null && url!.startsWith('http')) {
+                    DialogManager.showPreViewImageDialog(
+                        BackButtonBehavior.close, url);
+                  }
+                },
+                style: {
+                  "p": Style(fontSize: FontSize.large),
+                  "sentence": Style(
+                      textDecorationStyle: TextDecorationStyle.dashed,
+                      textDecorationColor: AppColors.THEME_COLOR),
+                  "hr": Style(
+                    margin: Margins.only(
+                        left: 0, right: 0, top: 10.w, bottom: 10.w),
+                    padding: EdgeInsets.all(0),
+                    border: Border(bottom: BorderSide(color: Colors.grey)),
+                  )
+                },
+                tagsList: Html.tags..addAll(['sentence']),
+              ),
+            )
+            /*Text(
               widget.testDetailResponse?.data![0].content??"",
               style: TextStyle(
                   fontWeight: FontWeight.w700,
@@ -108,7 +147,8 @@ class _ToOrderDetailPageState extends BasePageState<WritingPage> {
                   wordSpacing: 6.w,
                   height: 1.5,
                   color: Color(0xff353e4d)),
-            ),
+            )*/
+            ,
             _buildClassCard(0),
             _inputCard(),
           ],
@@ -177,44 +217,23 @@ class _ToOrderDetailPageState extends BasePageState<WritingPage> {
             borderRadius: BorderRadius.all(Radius.circular(10.w)),
             color: AppColors.c_FFFFFFFF),
         child: Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [_listShow(), _rightShow()],
         ),
       );
 
   Widget _listShow() => Expanded(
           child: Container(
-        width: 195.w,
-        height: 156.w,
-        child: ListView.builder(
-          itemCount: 6,
-          itemExtent: 26.w,
-          //列表项构造器
-          itemBuilder: (BuildContext context, int index) {
-            return Row(
-              children: [
-                Container(
-                  constraints: BoxConstraints(maxWidth: 50.w, minWidth: 50.w),
-                  child: Text(
-                    "主题：",
-                    style: TextStyle(
-                        fontWeight: FontWeight.w500,
-                        fontSize: 11.sp,
-                        color: Color(0xff353e4d)),
-                  ),
-                ),
-                Container(
-                  child: Text(
-                    "6月15日下午2:00—5:00",
-                    style: TextStyle(
-                        fontWeight: FontWeight.w500,
-                        fontSize: 11.sp,
-                        color: Color(0xff353e4d)),
-                  ),
-                ),
-              ],
-            );
-          },
-        ),
+        height: 180.w,
+        child: Text(exList,
+            style: TextStyle(
+                color: Color(0xff353e4d),
+                fontSize: 12.sp,
+                wordSpacing: 2.0,
+                height: 3),
+            overflow: TextOverflow.ellipsis,
+            maxLines: 5),
       ));
 
   Widget _rightShow() => Container(
@@ -228,6 +247,10 @@ class _ToOrderDetailPageState extends BasePageState<WritingPage> {
               onTap: () {
                 //RouterUtil.toNamed(AppRoutes.IntensiveListeningPage);
                 Util.toast("查看范文");
+                showDialog(
+                  context: context,
+                  builder: (context) => WritDialog(exTile, exList),
+                );
               },
               child: Image.asset(
                 R.imagesWritingLookBotton,
@@ -246,4 +269,66 @@ class _ToOrderDetailPageState extends BasePageState<WritingPage> {
           ],
         ),
       );
+
+  // 使用dialog 来自己真正的定义一个对话框
+  Widget myDialog(exList) {
+    return Dialog(
+      // insetPadding: EdgeInsets.all(10), //距离
+      shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(Radius.circular(20))),
+      //形状
+      backgroundColor: Colors.white,
+      clipBehavior: Clip.antiAlias,
+      //强制裁剪
+      elevation: 10,
+      child: Container(
+        //需要在内部限制下高度和宽度才能更好的显示
+        height: 250,
+        width: 300,
+        padding: EdgeInsets.all(18.w),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Row(
+              children: [
+                Image.asset(
+                  R.imagesWritingLookBotton,
+                  width: 77.w,
+                  height: 18.w,
+                ),
+                Expanded(child: Text('')),
+                InkWell(
+                    onTap: () {
+                      //RouterUtil.toNamed(AppRoutes.IntensiveListeningPage);
+
+                    },
+                    child: Image.asset(
+                      R.imagesToridClose,
+                      width: 12.w,
+                      height: 12.w,
+                    ))
+              ],
+            ),
+            Text(exTile,
+                style: TextStyle(
+                    color: Color(0xff353e4d),
+                    fontSize: 15.sp,
+                    wordSpacing: 2.0,
+                    height: 2),
+                overflow: TextOverflow.ellipsis,
+                maxLines: 5),
+            Text(exList,
+                style: TextStyle(
+                    color: Color(0xff353e4d),
+                    fontSize: 14.sp,
+                    wordSpacing: 2.0,
+                    height: 2),
+                overflow: TextOverflow.ellipsis,
+                maxLines: 5),
+          ],
+        ),
+      ),
+    );
+  }
 }
