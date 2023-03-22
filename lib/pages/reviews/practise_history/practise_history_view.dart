b76db7f5db89ffc9fd85064ce097ad7370e7081d
雷勋 <leixun33@163.com>
2023-03-22 11:07:21 +0800

    去掉timelines依赖

--- lib/pages/reviews/practise_history/practise_history_view.dart
+++ lib/pages/reviews/practise_history/practise_history_view.dart
@@ -5,7 +5,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
 import 'package:intl/intl.dart';
-import 'package:timelines/timelines.dart';
 
 import '../../../r.dart';
 import '../practtise_history/LeftLineWidget.dart';
