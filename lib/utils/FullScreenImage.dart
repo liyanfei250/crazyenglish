81b46bbeb1c6657e80f90b2c5c938f493cce1398
Your Name <467638186@qq.com>
2023-03-29 12:17:31 +0800

    新建班级的界面

---
+++ lib/utils/FullScreenImage.dart
@@ -0,0 +1,92 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+
+class FullScreenImage extends StatefulWidget {
+  final File? imageFile;
+
+  const FullScreenImage({Key? key, this.imageFile}) : super(key: key);
+
+  @override
+  _FullScreenImageState createState() => _FullScreenImageState();
+}
+
+class _FullScreenImageState extends State<FullScreenImage> {
+  double _scale = 1.0;
+  late Offset _position;
+
+  @override
+  void initState() {
+    super.initState();
+    _position =Offset.zero;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body:GestureDetector(
+        onScaleUpdate: (details) {
+          setState(() {
+            _scale = details.scale;
+            _position += details.focalPoint - details.localFocalPoint;
+          });
+        },
+        onTap: (){
+          Navigator.pop(context);
+        },
+        child: Stack(
+          children: [
+            Center(
+              child: Hero(
+                tag: 'imageHero',
+                child: Transform(
+                  transform: Matrix4.identity()
+                    ..translate(_position.dx, _position.dy)
+                    ..scale(_scale),
+                  child: Image.file(
+                    File(widget.imageFile!.path),
+                    fit: BoxFit.contain,
+                  ),
+                ),
+              ),
+            ),
+            /*Positioned(
+              top: 0,
+              right: 0,
+              child: IconButton(
+                icon: Icon(Icons.close, color: Colors.white),
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+              ),
+            ),
+            Positioned(
+              bottom: 0,
+              right: 0,
+              child: Row(
+                children: [
+                  IconButton(
+                    icon: Icon(Icons.zoom_in, color: Colors.white),
+                    onPressed: () {
+                      setState(() {
+                        _scale *= 1.5;
+                      });
+                    },
+                  ),
+                  IconButton(
+                    icon: Icon(Icons.zoom_out, color: Colors.white),
+                    onPressed: () {
+                      setState(() {
+                        _scale /= 1.5;
+                      });
+                    },
+                  ),
+                ],
+              ),
+            ),*/
+          ],
+        ),
+      ) ,
+    );
+  }
+}
