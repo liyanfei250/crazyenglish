0248ff1d607ac55b439f4e8f24958d6aa7c3f383
雷勋 <leixun33@163.com>
2023-05-23 09:46:35 +0800

    android证书添加 ios切为正式环境

--- lib/config.dart
+++ lib/config.dart
@@ -19,11 +19,6 @@ class Config {
   static const String PRODUCT_BUCKET_NAME = "prod-1315843937";
   static const String PRODUCT_API_DOMAIN = "https://api.crazyenglishweekly.com/fkyy";
 
-  static const String TEST_APP_ID = "1001";
-  static const String TEST_SECURITY_KEY = "5522e1d52361454c9f44eb7db1280f61";
-  static const String TEST_BUCKET_NAME = "test-1315843937";
-  static const String TEST_API_DOMAIN = "https://test-api.crazyenglishweekly.com/fkyy";
-
   static String get appId {
     switch (env) {
       case Env.NEIBU:
@@ -51,7 +46,7 @@ class Config {
       case Env.PRODUCT:
         return PRODUCT_API_DOMAIN;
       case Env.IOS:
-        return TEST_API_DOMAIN;
+        return PRODUCT_API_DOMAIN;
       default:
         return PRODUCT_API_DOMAIN;
     }
@@ -64,7 +59,7 @@ class Config {
       case Env.PRODUCT:
         return PRODUCT_BUCKET_NAME;
       case Env.IOS:
-        return TEST_BUCKET_NAME;
+        return PRODUCT_BUCKET_NAME;
       default:
         return PRODUCT_BUCKET_NAME;
     }
