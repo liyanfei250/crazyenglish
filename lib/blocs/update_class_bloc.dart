e3f0cb32dc3d7c50ba9bdf993c3de324d0d9364d
Your Name <467638186@qq.com>
2023-06-12 16:50:48 +0800

    新建班级的刷新处理

--- lib/blocs/update_class_bloc.dart
+++ lib/blocs/update_class_bloc.dart
@@ -1,7 +1,7 @@
 import 'package:crazyenglish/blocs/update_class_event.dart';
 import 'package:crazyenglish/blocs/update_class_state.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-
+//新建班级的刷新处理
 class UpdateClassBloc extends Bloc<ClassChangeEvent, ClassChangeState> {
   UpdateClassBloc() : super(ClassChangeInitial());
 
--- lib/blocs/update_collect_bloc.dart
+++ lib/blocs/update_collect_bloc.dart
@@ -2,16 +2,16 @@ import 'package:crazyenglish/blocs/update_collect_event.dart';
 import 'package:crazyenglish/blocs/update_collect_state.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
+//收藏取消的刷新处理
 class UpdateCollectBloc extends Bloc<CollectChangeEvent, CollectChangeState> {
   UpdateCollectBloc() : super(CollectChangeInitial());
 
-@override
-Stream<CollectChangeState> mapEventToState(
+  @override
+  Stream<CollectChangeState> mapEventToState(
     CollectChangeEvent event,
-    ) async* {
-  if(event is SendCollectChangeEvent){
-    yield CollectChangeSuccess();
+  ) async* {
+    if (event is SendCollectChangeEvent) {
+      yield CollectChangeSuccess();
+    }
   }
 }
-}
-
